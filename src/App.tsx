64b72c0749d145b17c8b649293fa7f0ba1d89674
maatina <tinanewa@gmail.com>
2024-12-06 13:32:58 -0500

    changes for aplify

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <div><h1> Hello from Amplify</h1></div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
